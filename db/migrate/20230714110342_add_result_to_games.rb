7d019c80154cd3b3e49963e9d61898dd2178f4b8
Jesper Persson <jesper.persson_96@hotmail.com>
2023-07-14 13:04:01 +0200

    added result to bet.

---
+++ db/migrate/20230714110342_add_result_to_games.rb
@@ -0,0 +1,5 @@
+class AddResultToGames < ActiveRecord::Migration[7.0]
+  def change
+    add_column :games, :result, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_14_104644) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_110342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_104644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "game_type"
+    t.string "result"
   end
 
   create_table "teams", force: :cascade do |t|
