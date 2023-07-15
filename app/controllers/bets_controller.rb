dc5c6f3ae8f0f2398417814a449b1e645d27d913
Jesper Persson <jesper.persson_96@hotmail.com>
2023-07-16 00:47:58 +0200

    added games controller and views.

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -58,14 +58,6 @@ class BetsController < ApplicationController
     ActiveRecord::Base.connection.execute(query)
   end
 
-  def games_by_date
-    Game.joins('JOIN teams AS home_teams ON home_teams.id = games.home_team_id')
-                         .joins('JOIN teams AS away_teams ON away_teams.id = games.away_team_id')
-                         .select("date, ARRAY_AGG(ARRAY[home_team_score::text, away_team_score::text, home_teams.name::text, away_teams.name::text, time::text]) AS games")
-                         .group(:date)
-                         .order(date: :asc)
-  end
-
   def latest_games
     latest_date = Game.where.not(home_team_score: nil, away_team_score: nil).order(date: :desc).limit(1).pluck(:date)
     Game.where(date: latest_date)
