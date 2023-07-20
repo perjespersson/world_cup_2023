65636dd8530e1b039ec79c138db6ca5dcc2de02e
Jesper Persson <jesper.persson_96@hotmail.com>
2023-07-20 12:19:09 +0200

    added previous games

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,7 +1,8 @@
 class BetsController < ApplicationController
   def index
     @table = table_query
-    @latest_games = latest_games
+    @current_games = current_games
+    @previous_games = previous_games
     @upcoming_games = upcoming_games
   end
 
@@ -9,71 +10,86 @@ class BetsController < ApplicationController
 
   def table_query
     query = <<-SQL
-              WITH user_games_with_result AS (
-                SELECT
-                  users.name,
-                  home_team_score,
-                  away_team_score,
-                  bets.bet,
-                  CASE
-                    WHEN home_team_score > away_team_score THEN '1'
-                    WHEN home_team_score = away_team_score THEN 'x'
-                    WHEN home_team_score < away_team_score THEN '2'
-                    ELSE NULL
-                  END AS result
-                FROM
-                  users
-                JOIN
-                  bets
-                ON
-                  bets.user_id = users.id
-                JOIN
-                  games
-                ON
-                  bets.game_id = games.id
-              ), user_games_with_points AS (
+                WITH user_games_with_result AS (
+                  SELECT
+                    users.name,
+                    home_team_score,
+                    away_team_score,
+                    bets.bet,
+                    CASE
+                      WHEN home_team_score > away_team_score THEN '1'
+                      WHEN home_team_score = away_team_score THEN 'x'
+                      WHEN home_team_score < away_team_score THEN '2'
+                      ELSE NULL
+                    END AS result
+                  FROM
+                    users
+                  JOIN
+                    bets
+                  ON
+                    bets.user_id = users.id
+                  JOIN
+                    games
+                  ON
+                    bets.game_id = games.id
+                ), user_games_with_points AS (
+                  SELECT
+                    *,
+                    CASE
+                      WHEN bet = result THEN 1
+                      WHEN bet != result THEN 0
+                      ELSE NULL
+                    END AS points
+                  FROM
+                    user_games_with_result
+                ), user_games_with_statistics AS (
+                  SELECT
+                    RANK () OVER (ORDER BY COALESCE(SUM(points), 0) DESC) AS rank,
+                    name,
+                    COUNT(*) filter (WHERE bet IS NOT NULL AND home_team_score IS NOT NULL AND away_team_score IS NOT NULL) total_bets,
+                    COUNT(*) filter (WHERE points = 1) AS wins,
+                    COUNT(*) filter (WHERE points = 0) AS losses,
+                    COALESCE(SUM(points), 0) AS points
+                  FROM
+                    user_games_with_points
+                  GROUP BY
+                    name
+                )
+
                 SELECT
                   *,
-                  CASE
-                    WHEN bet = result THEN 1
-                    WHEN bet != result THEN 0
-                    ELSE NULL
-                  END AS points
+                  ((wins::float / total_bets::float) * 100)::integer AS hit_rate
                 FROM
-                  user_games_with_result
-              )
-
-              SELECT
-                RANK () OVER (ORDER BY COALESCE(SUM(points), 0) DESC) AS rank,
-                name,
-                COUNT(*) filter (WHERE bet IS NOT NULL AND home_team_score IS NOT NULL AND away_team_score IS NOT NULL) total_bets,
-                COUNT(*) filter (WHERE points = 1) AS wins,
-                COUNT(*) filter (WHERE points = 0) AS losses,
-                COALESCE(SUM(points), 0) AS points
-              FROM
-                user_games_with_points
-              GROUP BY
-                name
+                  user_games_with_statistics
             SQL
 
     ActiveRecord::Base.connection.execute(query)
   end
 
   def current_date
-    @current_date ||= Game.where.not(home_team_score: nil, away_team_score: nil).order(date: :desc).limit(1).pluck(:date)
+    @current_date ||= Game.where(home_team_score: nil, away_team_score: nil).order(date: :asc).limit(1).pluck(:date)
+  end
+
+  def previous_date
+    @previous_date ||= Game.where("date < ?", current_date)
+                       .order(date: :desc)
+                       .limit(1)
+                       .pluck(:date)
   end
 
   def next_date
-    @next_date ||= Game.distinct(:date)
-                       .where("date > ?", current_date)
+    @next_date ||= Game.where("date > ?", current_date)
                        .order(date: :asc)
                        .limit(1)
                        .pluck(:date)
-
   end
 
-  def latest_games
+  def current_games
     Game.where(date: current_date).order(time: :asc)
+  end
+
+  def previous_games
+    Game.where(date: previous_date).order(time: :asc)
   end
 
   def upcoming_games
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,6 +21,7 @@ class GamesController < ApplicationController
   private
 
   def games_by_date
+    # TODO: Order by time aswell
     Game.joins('JOIN teams AS home_teams ON home_teams.id = games.home_team_id')
         .joins('JOIN teams AS away_teams ON away_teams.id = games.away_team_id')
         .select("date, ARRAY_AGG(ARRAY[home_team_score::text, away_team_score::text, home_teams.name::text, away_teams.name::text, time::text, home_teams.img::text, away_teams.img::text, games.id::text]) AS games")
