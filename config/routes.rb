5a94e63f0af61904f9ac177d064a3c9f263401c7
Jesper Persson <jesper.persson_96@hotmail.com>
2023-09-13 20:29:21 +0200

    added button for google sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+    omniauth_callbacks: 'users/omniauth_callbacks'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
