1ff70aac23e1304a08c0d311258e4c785f8d6acf
Jesper Persson <jesper.persson_96@hotmail.com>
2023-09-14 23:56:34 +0200

    more fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :bets
-
-  def self.from_omniauth(auth)
-    user = User.find_by(email: auth.info.email)
-
-    if user.nil?
-      User.create(
-                  email: auth.info.email,
-                  password: Devise.friendly_token[0, 20],
-                  name: auth.info.name,
-                  avatar_url: auth.info.image,
-                  provider: auth.provider,
-                  uid: auth.uid
-                )
-    elsif user.uid.nil? || user.provider.nil? # User has not signed up through google
-      return nil
-    else
-      return user
-    end
-  end
 end
